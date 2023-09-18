8d167f9cb0c323e57b13de6bd5a97293f032b3c2
gw31415 <git@amas.dev>
2023-09-19 03:07:59 +0900

    Basic Submission CRUD

--- server/app/submissions/views.py
+++ server/app/submissions/views.py
@@ -1,3 +1,25 @@
-# from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Submission
+from .serializer import SubmissionUpdateSerializer
+
+
+class SubmissionListView(generics.ListAPIView):
+    queryset = Submission.objects.all()
+
+
+class SubmissionCreateView(generics.CreateAPIView):
+    queryset = Submission.objects.all()
+
+
+class SubmissionRetrieveView(generics.RetrieveAPIView):
+    queryset = Submission.objects.all()
+
+
+class SubmissionUpdateView(generics.UpdateAPIView):
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionUpdateSerializer
+
+
+class SubmissionDeleteView(generics.DestroyAPIView):
+    queryset = Submission.objects.all()
